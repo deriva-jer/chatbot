ebd8f0cb34a4f7ecb7d91f44e7f8a7de99b858d4
deriva-jer <jeremy.gautama@derivaenergy.com>
2025-08-18 11:43:40 -0400

    Update old.py

--- old.py
+++ old.py
@@ -1,4 +1,4 @@
-import os
+Fimport os
 import json
 import uuid
 import boto3
@@ -140,7 +140,7 @@ if not create_dynamodb_table_if_not_exists():
     st.stop()
 
 # --- Streamlit Page ---
-st.set_page_config(page_title="Diva the Chatbot", layout="centered", initial_sidebar_state="expanded")
+st.set_page_config(page_icon = Image.open("deriva.jpg"), page_title="Diva the Chatbot", layout="centered", initial_sidebar_state="expanded")
 
 st.sidebar.title("⚙️ Settings")
 
